e50cdd0c371301a559286e5a8fc53ecd2d5fb4cd
Jeremia Henkel <henkel.jeremia@gmail.com>
2023-11-15 20:55:57 +0100

    second commit

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,3 +1,5 @@
+import "dotenv/config";
+import mongoose from "mongoose";
 import express from "express";
 const app = express();
 const port = 5000;
@@ -6,6 +8,8 @@ app.get("/", (req, res) => {
     res.send("Hello, World!");
 });
 
+mongoose.connect(process.env.MONGO_CONNNECTION_STRING)
+.then()
 app.listen(port, () => {
     console.log("Server running on port: " + port);
 });
