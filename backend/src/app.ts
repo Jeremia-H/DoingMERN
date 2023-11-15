d2621ea01d48595d3241fc4f78a8b9574465cf79
Jeremia Henkel <henkel.jeremia@gmail.com>
2023-11-15 21:47:27 +0100

    moved some stuff to newly created app.ts

---
+++ backend/src/app.ts
@@ -0,0 +1,10 @@
+import "dotenv/config";                                                 //for .env folder
+import express from "express";                                          //import express
+
+const app = express();                                                  //dont know what this does yet / calls the express function i guess
+
+app.get("/", (req, res) => {                                            
+    res.send("Hello, World!");                                          //This is what you see when you open the website, aka. get request
+});
+
+export default app;                                                     //expor the app const so we can import it in the server.ts
