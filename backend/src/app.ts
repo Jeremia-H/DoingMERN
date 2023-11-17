2f27126e3cd85645d534280468107e4513bd70fb
Jeremia Henkel <henkel.jeremia@gmail.com>
2023-11-17 17:39:07 +0100

    added more error handling with http-errors package

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,22 +1,31 @@
-import "dotenv/config";                                                 //for .env folder
+import "dotenv/config";                                                                                      //for .env folder
 import express, { NextFunction, Request, Response } from "express";                                          //import express
 import sensordatasRoutes from "./routes/sensordatas"
+import createHttpError, { isHttpError } from "http-errors";                                                  //createHttpError is a default immport, isHttpError not 
+import morgan from "morgan"
 
-const app = express();                                                  //dont know what this does yet / calls the express function i guess
+const app = express();                                                                                       //dont know what this does yet / calls the express function i guess
+
+app.use(morgan("dev"));                                                                                     //installed morgan for cooler logs
 
 app.use(express.json());
 
-app.use("/api/sensordata", sensordatasRoutes);                         //linking the sensordataroute
+app.use("/api/sensordata", sensordatasRoutes);                                                               //linking the sensordataroute
 
 app.use((req, res, next) => {
-    next(Error("Endpoint not found"));
-});
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-app.use((error: unknown, req: Request, res: Response, next: NextFunction) => {          //moves error handling from a try catch stateent into a the error handler of express
-    console.error(error);                                           //prints the error into the console
-    let errorMessage = "An Unknown error occurred";                  //create variable errorMessage with a default String, this can be cahnged by the error itself
-    if (error instanceof Error) errorMessage = error.message;       //We have to verify if the error is actually and error, because the catch function catches everythign unknown
-    res.status(500).json({ error: errorMessage });                   //give out a error response and includes the error Message as Json
+    next(createHttpError(404, "Endpoint not found"));                                                        //Error handling via the http-errors package
 });
 
-export default app;                                                     //expor the app const so we can import it in the server.ts
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+app.use((error: unknown, req: Request, res: Response, next: NextFunction) => {                               //moves error handling from a try catch stateent into a the error handler of express
+    console.error(error);                                                                                    //prints the error into the console
+    let errorMessage = "An Unknown error occurred";                                                          //create variable errorMessage with a default String, this can be cahnged by the error itself
+    let statusCode = 500;
+    if (isHttpError(error)) {
+        statusCode = error.status;                                          
+        errorMessage = error.message;
+    }                                                                                                        //We have to verify if the error is actually and error, because the catch function catches everythign unknown
+    res.status(statusCode).json({ error: errorMessage });                                                    //give out a error response and includes the error Message as Json
+});
+
+export default app;                                                                                          //expor the app const so we can import it in the server.ts
--- backend/src/controllers/sensordatas.ts
+++ backend/src/controllers/sensordatas.ts
@@ -1,21 +1,30 @@
 import { RequestHandler } from "express";
-import sensordataModel from "../models/sensordata";                             //It doesnt matter what we call the import, it just imports what we sepcified in the sensordata.ts as export
+import sensordataModel from "../models/sensordata";                                                                                       //It doesnt matter what we call the import, it just imports what we sepcified in the sensordata.ts as export
+import createHttpError from "http-errors";
+import mongoose from "mongoose";
 
-export const getSensorDatas: RequestHandler = async (req, res, next) => {                                      //changed this to asnyc so we can use await in the function.
-    try {                                                               //Error Handling!
-        // throw Error("Bazinga!");                                     //Trial Error
-        const sensordatas = await sensordataModel.find().exec();            //With this we get the data from the database
-        res.status(200).json(sensordatas);                                  // this catches the response from the await above as ok and then gives out the sensordata as json    
-    } catch (error) {                                                   //Error Handling Part 2
-        next(error);                                                    // give the error over to the next route.
+export const getSensorDatas: RequestHandler = async (req, res, next) => {                                                                 //changed this to asnyc so we can use await in the function.
+    try {                                                                                                                                 //Error Handling!
+        // throw Error("Bazinga!");                                                                                                       //Trial Error
+        const sensordatas = await sensordataModel.find().exec();                                                                          //With this we get the data from the database
+        res.status(200).json(sensordatas);                                                                                                // this catches the response from the await above as ok and then gives out the sensordata as json    
+    } catch (error) {                                                                                                                     //Error Handling Part 2
+        next(error);                                                                                                                      // give the error over to the next route.
     }
 };
 
-export const getSensorData: RequestHandler =async (req, res, next) => {                     //This function will handle specific datapoints by using the ID mongoDB gives every entry
-    const sensordataID = req.params.sensordataID;                                           //This uses the ID we specify in the routes file after the : router.get("/:sensordataID"
+export const getSensorData: RequestHandler =async (req, res, next) => {                                                                   //This function will handle specific datapoints by using the ID mongoDB gives every entry
+    const sensordataID = req.params.sensordataID;                                                                                         //This uses the ID we specify in the routes file after the : router.get("/:sensordataID"
     
     try {
-        const sensordata = await sensordataModel.findById(sensordataID).exec();             //THis is similar to getting all data but now we can use the express function to just get a singular datapoint by its ID, which we use the const for
+        if (!mongoose.isValidObjectId(sensordataID)) {
+            throw createHttpError(400, "Invalid sensordata ID")                                                                           // if the sensordataID is invalid we check here and give an error
+        }
+
+        const sensordata = await sensordataModel.findById(sensordataID).exec();                                                           //THis is similar to getting all data but now we can use the express function to just get a singular datapoint by its ID, which we use the const for
+        if (!sensordata) {                                                                                                                // doing error handlign if the sensordata is not found / null / unkown / etc.
+            throw createHttpError(404, "Sensordata not found");                                                                           // this only throws an error if the string is similar to a real one, not if you only e.g. put "123" then you still get unkown error, for that we check earlier
+        }
         res.status(200).json(sensordata);
     } catch (error) {
         next(error);
@@ -23,17 +32,26 @@ export const getSensorData: RequestHandler =async (req, res, next) => {         
     }
 }
 
-export const createSensorData: RequestHandler = async (req, res, next) => {         //This Handler will be able to create new Data in our db
-    const sensorname = req.body.sensorname;                                         //telling it to use the sensorname we send to it via http.post
-    const grad = req.body.grad;                                                     //telling it to use the grad we send to it via http.post
+interface CreateSensorDataBody {                                                                                                          // Creating an interface so we can check ourselves if the Body Data is actually a string, we will also use this to check if the sensorname is actually included 
+    sensorname?: string,
+    grad?: string,
+}
+
+export const createSensorData: RequestHandler<unknown, unknown, CreateSensorDataBody, unknown> = async (req, res, next) => {              //This Handler will be able to create new Data in our db, we pass the CreateSensorDataBody in the RequestHandler
+    const sensorname = req.body.sensorname;                                                                                               //telling it to use the sensorname we send to it via http.post
+    const grad = req.body.grad;                                                                                                           //telling it to use the grad we send to it via http.post
 
     try {
-        const newSensordata = await sensordataModel.create({                        //In here we now say we want to create a new entry in the db with the consts we created above
+        if (!sensorname) {                                                                                                                // !sensorname means if the sensorname is false, which it is when it is undefined
+            throw createHttpError(400, "data must have a sensorname");                                                                    // then we throw a 400 error
+        }
+
+        const newSensordata = await sensordataModel.create({                                                                              //In here we now say we want to create a new entry in the db with the consts we created above
             sensorname: sensorname,
             grad: grad
         });
 
-        res.status(201).json(newSensordata)                                            // saying new resource has been created
+        res.status(201).json(newSensordata)                                                                                               // saying new resource has been created
     } catch (error) {
         next(error);
         
