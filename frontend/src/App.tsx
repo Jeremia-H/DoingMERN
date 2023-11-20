79fece239e6e9916db29529ac698bcbf25943563
Jeremia Henkel <henkel.jeremia@gmail.com>
2023-11-20 16:01:51 +0100

    Added the timestamps to notes and did some styling

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,31 +1,36 @@
 import React, { useEffect, useState } from 'react';
-import { SensorData as SensorDataModel} from './models/sensordata';                                    //alias so it does nto get so confusing
+import { SensorData as SensorDataModel} from './models/sensordata';                                            //alias so it does nto get so confusing
 import  SensorDataComponent from './components/SensorData';
+import { Col, Container, Row } from 'react-bootstrap';
+import styles from "./styles/SensorDataPage.module.css"
 
 function App() {
-  const[sensordata, setSensorData] = useState<SensorDataModel[]>([]);                                  //We make an array for the useState function and thenn tell it that it should be an empty array and also that sensordata is supposed to be of the datatype of our model as an array
+  const[sensordata, setSensorData] = useState<SensorDataModel[]>([]);                                          //We make an array for the useState function and thenn tell it that it should be an empty array and also that sensordata is supposed to be of the datatype of our model as an array
 
-  useEffect(() => {                                                                                    // with use effect we can setup a function that should run outside of the site rendering, so it does not run every time we render
-    async function loadSensordatas() {                                                                 //we need to use an async function inside of the useEffect function because thats just how it works
+  useEffect(() => {                                                                                            // with use effect we can setup a function that should run outside of the site rendering, so it does not run every time we render
+    async function loadSensordatas() {                                                                         //we need to use an async function inside of the useEffect function because thats just how it works
     try {
-      const response = await fetch("http://localhost:5000/api/sensordata", { method: "GET"});          //this is the link we setup in the backend to get all of our sensordata, so we use a get request
-      const sensordatas = await response.json();                                                       // we then parse the response as json into the sensordatas const and give that to the setSensorData function we created outside of this
+      const response = await fetch("http://localhost:5000/api/sensordata", { method: "GET"});                  //this is the link we setup in the backend to get all of our sensordata, so we use a get request
+      const sensordatas = await response.json();                                                               // we then parse the response as json into the sensordatas const and give that to the setSensorData function we created outside of this
       setSensorData(sensordatas);
     } catch (error) {
       console.error(error);
       alert(error);
     }
     }
-    loadSensordatas();                                                                                 // dont forget to call the function because we had to put it inside of another function
-  }, []);                                                                                              //we pass this array because this makes it so the useEffect only runs once
+    loadSensordatas();                                                                                         // dont forget to call the function because we had to put it inside of another function
+  }, []);                                                                                                      //we pass this array because this makes it so the useEffect only runs once
 
   return (
-    <div>
-      {sensordata.map(sensordata => (                                                                  //we map the sensordata we got in our function up here to the Component we created (SensorData.tsx)
-        <SensorDataComponent sensordataInsideProps={sensordata} key={sensordata._id}/>                 // since we made an interface in the component and told that it should use the model as base, we can now just pass the sensordata we got here ( which is the same model) 1 to 1 over.
-                                                                                                       //and then we also create a key, this should be unique, so we just use the unique ID mongodb gives it
+    <Container>
+      <Row xs={1} md={2} xl={3} className='g-4'>                                                        {/*this changes the layout for screen-size and also applies the bootstrap g-4 styling*/}                               
+      {sensordata.map(sensordata => (                                                                          //we map the sensordata we got in our function up here to the Component we created (SensorData.tsx)
+      <Col key={sensordata._id}>
+        <SensorDataComponent sensordataInsideProps={sensordata} className={styles.sensordata}/>        {/*since we made an interface in the component and told that it should use the model as base, we can now just pass the sensordata we got here ( which is the same model) 1 to 1 over.*/}                   
+        </Col>                                                                                                 //and then we also create a key, this should be unique, so we just use the unique ID mongodb gives it  
       ))}
-    </div>
+      </Row>
+    </Container>
   );
 }
 
--- frontend/src/components/SensorData.tsx
+++ frontend/src/components/SensorData.tsx
@@ -1,21 +1,30 @@
 import styles from "../styles/SensorData.module.css"
 import { Card} from "react-bootstrap";
 import { SensorData as SensorDataModel } from "../models/sensordata";
+import { formatDate } from "../utils/formatDate";
 
 interface SensorDataProps {                                             // creating an interface with our model that we created, so we can use the data from the model in the card
     sensordataInsideProps: SensorDataModel,
+    className?: string,
 }
 
-const SensorData = ({ sensordataInsideProps} : SensorDataProps) => {               //Destructoring so we can just use the sensordata direcly out of the Props instead of always having to write SensorDataProps.sensordata for example
+const SensorData = ({ sensordataInsideProps, className} : SensorDataProps) => {               //Destructoring so we can just use the sensordata direcly out of the Props instead of always having to write SensorDataProps.sensordata for example
     const {
         sensorname,
         grad,
         createdAt,
         updateAt,
     } = sensordataInsideProps                                                       //Destructoring so we can just use the sensordata direcly out of the Props instead of always having to write sensordata.sensorname for example
+
+    let createdUpdatedText: string;
+    if (updateAt > createdAt) {
+        createdUpdatedText = "Updated: " + formatDate(updateAt);
+    } else {
+        createdUpdatedText = "Updated: " + formatDate(createdAt);
+    }
     return (
-        <Card className={styles.sensordataCard}>
-            <Card.Body>
+        <Card className={`${styles.sensordataCard} ${className}`}>
+            <Card.Body className={styles.cardBody}>
                 <Card.Title>
                     {sensorname}
                 </Card.Title>
@@ -23,6 +32,9 @@ const SensorData = ({ sensordataInsideProps} : SensorDataProps) => {            
                     {grad}
                 </Card.Text>
             </Card.Body>
+            <Card.Footer className="text-muted">
+                    {createdUpdatedText}
+                </Card.Footer>
         </Card>
     )
 }
